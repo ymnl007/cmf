ab08e4042570e04b7296f82e06a9510958d594e2
chenfeng <chenfeng@ihuilian.com>
2014-10-21 15:05:15 +0800

    测试行提交2

--- Ymnl/Admin/Widget/MenuWidget.class.php
+++ Ymnl/Admin/Widget/MenuWidget.class.php
@@ -6,10 +6,13 @@ namespace Admin\Widget;
 use Think\Controller;
 class MenuWidget extends Controller{
 	public function index(){
-		
+
 		return $menu;
 	}
 	public function add(){
 
+	}
+	public function del(){
+		
 	}
 }
